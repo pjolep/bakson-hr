ff3aac69b1b212f00df51e05af2e7aeaa9f88efd
Pavle Paunovic <paavlee@yahoo.com>
2015-10-22 10:34:15 +0200

    Employee data

---
+++ app/directives/employeeData.js
@@ -0,0 +1,10 @@
+angular.module('directives').directive('employeeData', function(uiProvider){
+    return {
+        restrict : 'AE',
+        templateUrl : 'view/templates/employeeData.html',
+        link : function(scope){
+            
+
+        }
+    }
+});
