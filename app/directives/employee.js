9830372ac23df92178c34a162521dea8ee211e2b
Pavle Paunovic <paavlee@yahoo.com>
2015-10-22 10:33:48 +0200

    Employees directive

---
+++ app/directives/employee.js
@@ -0,0 +1,10 @@
+angular.module('directives').directive('employee', function(uiProvider){
+    return {
+        restrict : 'AE',
+        templateUrl : 'view/templates/employee.html',
+        link : function(scope){
+            
+
+        }
+    }
+});
