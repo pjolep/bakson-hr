fe2cceaa1d8a6e18e7357da31aa587c06af86d75
Pavle Paunovic <paavlee@yahoo.com>
2015-10-22 10:21:17 +0200

    Added MongoDb

--- express.js
+++ express.js
@@ -6,10 +6,20 @@ var _ = require('underscore')._;
 var path = require('path');
 var jsonfile = require('jsonfile');
 var basicAuth = require('basic-auth');
+var config = require('./config/config.json');
 
-var file, dir;
-var arr = [];
+
+var file, dir = __dirname;
+var data = [], absentData;
 var dirs = __dirname.split('/');
+
+
+
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert'),
+    ObjectID = require('mongodb').ObjectID;
+
+var list = path.join(__dirname, '/../..');
 
 for(var i = 0; i < dirs.length; i++){
     if(dirs[i] != 'dist'){
@@ -20,7 +30,6 @@ for(var i = 0; i < dirs.length; i++){
         file = 'data.json';
     }
 }
-
 
 
 var auth = function (req, res, next) {
@@ -35,7 +44,7 @@ var auth = function (req, res, next) {
         return unauthorized(res);
     }
 
-    if (user.name === '123' && user.pass === '123') {
+    if (user.name === config.username && user.pass === config.password) {
         return next();
     } else {
         return unauthorized(res);
@@ -46,48 +55,105 @@ app.get('/', auth, function(req, res) {
     res.sendFile(path.join(dir + 'index.html'))
 });
 
-
 app.use(express.static(dir));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || config.port);
 
+var url =
+    'mongodb://localhost:27017/Employees'
+    || process.env.MONGOLAB_URI
+    || process.env.MONGOHQ_URL;
 
-// Read file, parse it, append data to array, serialize, replace file content.
+MongoClient.connect(url, function(err, db) {
 
-// 1. Read file
+    if(err) throw err;
 
-fs.readFile(file, function(err, data){
-    if(err){
-        throw err;
-    }
-// 2. Parse it
-// 3. Serialize it
-    arr = JSON.parse(data);
+    var selectAll = function(db, collection, onFinish) {
+        data = [];
+        var cursor = db.collection(collection).find( );
+        cursor.each(function(err, doc) {
+            assert.equal(err, null);
+            if (doc != null) {
+                data.push(doc);
+            } else {
+                onFinish(data);
+            }
+        });
+    };
+
+    var selectLast = function(db, collection, onFinish) {
+        data = [];
+        var cursor = db.collection(collection).find().sort({$natural: -1}).limit(1);
+        cursor.each(function(err, doc) {
+            assert.equal(err, null);
+            if (doc != null) {
+                data.push(doc);
+            } else {
+                onFinish(data);
+            }
+        });
+    };
+
+    app.post('/addNewEmployee', function(req, res) {
+        db.collection('employees').insert(req.body);
+        res.send(req.body);
+    });
+
+    app.post('/saveEmployeeData', function(req, res) {
+        var arr = [];
+        req.body.Absentee.forEach(function(val){
+            arr.push({
+                //_id : ObjectID(req.body._id),
+                date : val.date,
+                dateTo : val.dateTo,
+                typeOfVacation : val.typeOfVacation,
+                comment : val.comment
+            });
+        });
+
+        db.collection('employees').update(
+            {_id:ObjectID(req.body._id)},
+                {$set: {
+                    Adresa: req.body.Adresa,
+                    DatumRodjenja: req.body.DatumRodjenja,
+                    BrojDanaOdmora: req.body.BrojDanaOdmora,
+                    Komentar: req.body.Komentar,
+                    Firma: req.body.Firma,
+                    Absentee : arr
+                }
+            }
+        );
+        res.sendStatus(200);
+    });
+
+    app.post('/removeAbsenteeRow', function(req, res) {
+        db.collection('employees').remove( {_id: ObjectID(req.body._id)});
+        var id = req.body._id;
+        req.body._id = ObjectID(id);
+        db.collection('employees').insert(req.body);
+        res.sendStatus(200);
+    });
+
+    app.get('/getLastSavedEmployee', function(req, res) {
+        selectLast(db, 'employees', function(data) {
+            res.setHeader('Content-type', 'application/json');
+            res.send(data);
+        });
+    });
+
+    app.get('/fetchEmployees', function(req, res) {
+        assert.equal(null, err);
+        selectAll(db, 'employees', function(data) {
+            res.setHeader('Content-type', 'application/json');
+            res.send(data);
+        });
+    });
+
+    app.post('/removeEmployee', function(req, res) {
+        db.collection('employees').remove( {_id: ObjectID(req.body._id)});
+        res.sendStatus(200);
+    });
+
 });
-
-app.post('/save', function(req, res) {
-    for(var i = 0; i < arr.length; i++){
-        if(arr[i].Name === req.body.Name){
-            arr.splice(i, 1);
-        }
-    }
-    arr.push(req.body);
-
-// 4. Replace file content
-
-    jsonfile.writeFile(file, arr, {spaces : 2}, function(err){});
-    res.sendStatus(200);
-});
-
-app.post('/remove', function(req, res) {
-    for(var i = 0; i < arr.length; i++){
-        if(arr[i].Name === req.body.Name){
-            arr.splice(i, 1);
-        }
-    }
-    jsonfile.writeFile(file, arr, {spaces : 2}, function(err){});
-    res.sendStatus(200);
-});
-
 app.listen(app.get('port'));
