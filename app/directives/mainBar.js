5974bd31a4c914e45f89447bf395ef38d01bd95f
Pavle Paunovic <paavlee@yahoo.com>
2015-10-22 10:35:17 +0200

    mainBar

---
+++ app/directives/mainBar.js
@@ -0,0 +1,25 @@
+angular.module('directives').directive('mainBar', function(){
+    return {
+        restrict : 'AE',
+        templateUrl : 'view/templates/mainBar.html',
+        link : function(scope, el, attr){
+
+        }
+    }
+});
+
+
+
+angular.module('directives').directive('resizable', function($window) {
+    return function($scope) {
+        $scope.initializeWindowSize = function() {
+            $scope.windowHeight = $window.innerHeight;
+            return $scope.windowWidth = $window.innerWidth;
+        };
+        $scope.initializeWindowSize();
+        return angular.element($window).bind('resize', function() {
+            $scope.initializeWindowSize();
+            return $scope.$apply();
+        });
+    };
+});
